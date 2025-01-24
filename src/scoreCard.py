7b389840d713401d9e66a291a46957d741377703
diegoguntinn <diego232005@gmail.com>
2025-01-24 11:14:14 +0100

    para casa

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -1,20 +1,30 @@
 class ScoreCard:
 
-    def __init__(self, pins):
-        self.PINS =list(pins)
+    def __init__(self, pins: str):
+        self.PINS =str(pins)
 
-    #def get_score(self):
-    #   return sum(int(digito) for digito in self.PINS)
+    def get_cero(self):
+        self.PINS = self.PINS.replace('-', "0")
+        resultado = []
+        for posicion, digito in enumerate(self.PINS):
+            if digito == "/":
+                valor_semipleno= 10 - self.PINS[posicion -1]
+                valor_siguiente_numero = self.PINS[posicion + 1]
+                resultado.append(valor_semipleno)
+                print(resultado)
+                resultado.append(valor_siguiente_numero)
+                print(resultado)
+        #self.PINS = ''.join(str(10 - int(self.PINS[self.PINS.index(digito)-1] ) + int(self.PINS[self.PINS.index(digito)+1] )) if digito == "/" else digito for digito in self.PINS)
+
+#        self.PINS = ''.join(str(10 + int(self.PINS[self.PINS.index(digito)+1] ) + int(self.PINS[self.PINS.index(digito)+2] )) if digito == "X" else digito for digito in self.PINS)
+        resultado.append(digito)
+        return resultado
     
-    def get_score(self):
-        numero = [int(digito) for digito in self.PINS if digito.isdigit()]
-        return sum(numero)
+    #def get_score(self):
+     #   return sum(int(digitos)for digitos in self.get_cero())
 
-    def spare_not_extra(self):
-        tirada = 0
-        for  numero in self.PINS:
-            if numero == "/":
-                tirada = self.PINS[self.PINS.index(numero)] = 10 - int(self.PINS[self.PINS.index(numero)-1] ) + int(self.PINS[self.PINS.index(numero)+1] )
-                numero.replace("/",str(tirada))
-        return sum(int(digito) for digito in self.PINS)  
-        
+    #def spare_not_extra(self):
+      #  return sum(int(digito) for digito in self.get_cero())
+    
+    #def get_strike(self):
+     #   return sum(int(digito) for digito in self.get_cero())
--- test/test_score_card.py
+++ test/test_score_card.py
@@ -2,27 +2,75 @@ import pytest
 
 from src.scoreCard import ScoreCard
 
-def test_get_score():
-    PINS = "12345123451234512345"
-    total = 60
-    pins = ScoreCard(PINS)
-    assert pins
-    assert pins.get_score() == total
 
-def test_symbol_zero():
+def test_get_cero():
     PINS = "9-9-9-9-9-9-9-9-9-9-"
-    total = 90
+    total = "90"
     pins = ScoreCard(PINS)
-    assert pins.get_score() == total
+    assert pins.get_cero() == total
 
-    PINS = "9-3561368153258-7181"
-    total = 82
-    pins = ScoreCard(PINS)
-    assert pins.get_score() == total
-
-
-def test_spare_not_extra():
     PINS = "4/444444444444444444"
-    total = 86
+    total = "86"
     pins = ScoreCard(PINS)
-    assert pins.spare_not_extra() == total
+    assert pins.get_cero() == total
+
+#    PINS = "X999999999999999999"
+#    total = "28999999999999999999"
+#    pins = ScoreCard(PINS)
+#    assert pins.get_cero() == total
+#
+#    PINS = "X9-99/9999999999999"
+#    total = "199099109999999999999"
+#    pins = ScoreCard(PINS)
+#    assert pins.get_cero() == total
+#
+#    PINS = "9-3/613/815/-/8-7/8-"
+#    total = "90313613"
+#    pins= ScoreCard(PINS)
+#    assert pins.get_cero() == total
+#
+#    PINS = "X9-9-9-9-9-9-9-9-9-"
+#    total = "19909090909090909090"
+#    pins = ScoreCard(PINS)
+#    assert pins.get_cero() == total
+ 
+# def test_get_score():
+#    PINS = "12345123451234512345"
+#    total = 60
+#    pins = ScoreCard(PINS)
+#    assert pins
+#    assert pins.get_score() == total
+
+#def test_symbol_zero():
+ #   PINS = "9-9-9-9-9-9-9-9-9-9-"
+  #  total = 90
+   # pins = ScoreCard(PINS)
+    #assert pins.get_score() == total
+
+#    PINS = "9-3561368153258-7181"
+ #   total = 82
+  #  pins = ScoreCard(PINS)
+   # assert pins.get_score() == total
+
+
+# def test_spare_not_extra():
+#     PINS = "9-3/613/815/-/8-7/8-"
+#     total = 121
+#     pins= ScoreCard(PINS)
+#     assert pins.spare_not_extra() == total
+
+# def test_strike():
+#     PINS = "X9-9-9-9-9-9-9-9-9-"
+#     total = 100
+#     pins = ScoreCard(PINS)
+#     assert pins.get_strike() == total
+# #def test_strike():
+ #   PINS = "X9-9-9-9-9-9-9-9-9-"
+  #  total = 100
+   # pins = ScoreCard(PINS)
+    #assert pins.get_strike() == total
+
+    #PINS = "X9-X9-9-9-9-9-9-9-"
+    #total = 110
+    #pins = ScoreCard(PINS)
+    #assert pins.get_strike() == total
